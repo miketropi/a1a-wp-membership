d77c7eb53132111308d3047e4d9b4b7192f5a5f1
Hiếu Huỳnh <mike.beplus@gmail.com>
2025-03-02 20:12:15 +0700

    Fix a1a_dashboard_page non

--- inc/custom-routes.php
+++ inc/custom-routes.php
@@ -7,6 +7,8 @@ add_action( 'init', 'a1am_custom_rewrite_rule', 10, 0 );
 
 function a1am_custom_rewrite_rule() { 
   $dashboard_page = get_field('a1a_dashboard_page', 'option');
+  if(!$dashboard_page) return; 
+
   $dashboard_root = $dashboard_page->post_name;
 
   add_rewrite_rule(
